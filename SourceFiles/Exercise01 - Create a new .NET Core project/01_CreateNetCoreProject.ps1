16247aec6a6ffa8e3e171baf217eb469fc74bd03
Ted Pattison <ted@tedpattison.net>
2020-08-26 23:04:27 -0400

    Updates

--- SourceFiles/Exercise01 - Create a new .NET Core project/01_CreateNetCoreProject.ps1
+++ SourceFiles/Exercise01 - Create a new .NET Core project/01_CreateNetCoreProject.ps1
@@ -2,6 +2,6 @@ dotnet new mvc --auth SingleOrg --framework netcoreapp3.1
 
 dotnet remove package Microsoft.AspNetCore.Authentication.AzureAD.UI
 
-dotnet add package Microsoft.Identity.Web -v 0.2.3-preview
-dotnet add package Microsoft.Identity.Web.UI -v 0.2.3-preview
+dotnet add package Microsoft.Identity.Web -v 0.3.0-preview
+dotnet add package Microsoft.Identity.Web.UI -v 0.3.0-preview
 dotnet add package Microsoft.PowerBi.Api
