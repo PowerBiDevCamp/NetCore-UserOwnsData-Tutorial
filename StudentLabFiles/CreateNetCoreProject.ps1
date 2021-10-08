0c319df1c831f388a442253a9b9a863b23f4fe6f
Ted Pattison <ted@tedpattison.net>
2021-10-08 12:56:12 -0400

    Updates

--- StudentLabFiles/CreateNetCoreProject.ps1
+++ StudentLabFiles/CreateNetCoreProject.ps1
@@ -2,7 +2,7 @@ dotnet new mvc --auth SingleOrg --framework netcoreapp3.1
 
 dotnet remove package Microsoft.AspNetCore.Authentication.AzureAD.UI
 
-# update to latest available version of Microsoft.Identity.Web
-dotnet add package Microsoft.Identity.Web -v 0.3.0-preview
-dotnet add package Microsoft.Identity.Web.UI -v 0.3.0-preview
+# install latest available version of Microsoft.Identity.Web and Power BI .NET SDK
+dotnet add package Microsoft.Identity.Web
+dotnet add package Microsoft.Identity.Web.UI
 dotnet add package Microsoft.PowerBi.Api
